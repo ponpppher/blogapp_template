5c33aaaf2edef2030daffe0aab281ec82fccef4d
ponpppher <genmaier@gmail.com>
2018-12-22 19:15:43 +0900

    add login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root to: "blogs#index"
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "blogs#index"
+  devise_for :users
 end
